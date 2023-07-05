26347e353ba7a364702c66049e87862da3e6dafd
src3453 <103661526+src3453@users.noreply.github.com>
2023-07-05 14:37:06 +0900

    "

--- multiNotify/lineOfficialAccount.py
+++ multiNotify/lineOfficialAccount.py
@@ -1,12 +1,16 @@
 import requests
+class lineOfficialAccount:
+    def __init__(self,token:str,timeout:int=10) -> None:
+        self.token = token
+        self.timeout = timeout
 
-def broadcast(token:str,messages:dict[str],uuid:str=None,timeout:int=10):
-    ch_token = token
-    line_notify_api = 'https://api.line.me/v2/bot/message/broadcast'
-    headers = [{'Content-Type': 'application/json'},{'Authorization': f'Bearer {ch_token}'},{'X-Line-Retry-Key':uuid}]
-    data = {"messages":messages}
-    return requests.post(line_notify_api, headers = headers, data = data,timeout=timeout)
-def get_quota(token:str,timeout:int=10):
-    api="https://api.line.me/v2/bot/message/quota"
-    headers={"Authorization": f"Bearer {token}"}
-    return requests.get(api,headers=headers,timeout=timeout)
+    def broadcast(self,messages:dict[str],uuid:str=None):
+        ch_token = self.token
+        line_notify_api = 'https://api.line.me/v2/bot/message/broadcast'
+        headers = [{'Content-Type': 'application/json'},{'Authorization': f'Bearer {ch_token}'},{'X-Line-Retry-Key':uuid}]
+        data = {"messages":messages}
+        return requests.post(line_notify_api, headers = headers, data = data,timeout=self.timeout)
+    def get_quota(self):
+        api="https://api.line.me/v2/bot/message/quota"
+        headers={"Authorization": f"Bearer {self.token}"}
+        return requests.get(api,headers=headers,timeout=self.timeout)
