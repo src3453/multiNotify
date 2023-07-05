e3bc2957744b0f85cb16cf5d0b9b65a721e2fc1f
src3453 <103661526+src3453@users.noreply.github.com>
2023-07-05 14:34:36 +0900

    classize

--- multiNotify/discordWebHook.py
+++ multiNotify/discordWebHook.py
@@ -1,9 +1,14 @@
 import requests
 import json
 
-def send(webhook_url: str,text: str,embeds:dict=None,timeout:int=10):
-    main_content = {'content': text,'embeds': embeds}
-    headers      = {'Content-Type': 'application/json'}
+class discord:
+    class webhook:
+            def __init__(self,url:str,timeout:int=10) -> None:
+                self.url = url
+                self.timeout=timeout
+            def send(self,text:str,embeds:dict=None):
+                main_content = {'content': text,'embeds': embeds}
+                headers      = {'Content-Type': 'application/json'}
 
-    response     = requests.post(webhook_url, json.dumps(main_content), headers=headers,timeout=timeout)
-    return response
+                response     = requests.post(self.url, json.dumps(main_content), headers=headers,timeout=self.timeout)
+                return response
--- multiNotify/lineNotify.py
+++ multiNotify/lineNotify.py
@@ -1,8 +1,12 @@
 import requests
-
-def notify(token:str,notification_message:str,file:bytes=None,timeout:int=10):
-    line_notify_token = token
-    line_notify_api = 'https://notify-api.line.me/api/notify'
-    headers = {'Authorization': f'Bearer {line_notify_token}'}
-    data = {'message': f'message: {notification_message}'}
-    requests.post(line_notify_api, headers = headers, data = data, files = file, timeout=timeout)
+class lineNotify:
+    def __init__(self,token:str,timeout:int=10) -> None:
+        self.token = token
+        self.timeout=timeout
+    def notify(self,notification_message:str,file:bytes=None) -> requests.Response:
+        line_notify_token = self.token
+        line_notify_api = 'https://notify-api.line.me/api/notify'
+        headers = {'Authorization': f'Bearer {line_notify_token}'}
+        data = {'message': f'message: {notification_message}'}
+        r = requests.post(line_notify_api, headers = headers, data = data, files = file, timeout=self.timeout)
+        return r
